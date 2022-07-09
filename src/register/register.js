eb740b7d61df4b59e5cf522db81053059d816080
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-07-09 22:30:36 +0900

    registerページを修正

---
+++ src/register/register.js
@@ -0,0 +1,21 @@
+const password = document.getElementById('password')
+const address = document.getElementById('address')
+const button = document.getElementById('submit')
+
+const createId = async () => {
+  const values = {
+    psw: password.value,
+    ad: address.value,
+  }
+  const res = await fetch('', {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(values),
+  })
+  const json = await res.json()
+  document.cookie = `userId=${json.userId}`
+  location.pathname = `/my-page.html`
+}
